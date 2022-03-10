9d37f105a0a0f96872f77c836fc302975f256fd2
Frank Taplin <ftaplin@wsutech.edu>
2022-03-09 21:52:08 -0600

    CAC-1025

---
+++ exercises/exercise3.py
@@ -0,0 +1,29 @@
+# This is the section for exercise 2-3
+
+name_section1 = 'Hal'
+print(f"Open the pod bay doors, {name_section1}.")
+print()
+
+# This is the section for exercise 2-4
+# I am not capitalizing my middle name in the variable as well as starting by printing
+# it out first to show the results of the title argument.
+name_section2 = 'Frank lloyd Taplin'
+print(name_section2)
+print(name_section2.lower())
+print(name_section2.upper())
+print(name_section2.title())
+print()
+
+# This is the section for exercise 2-5
+print('Franklin D. Roosevelt once said "We have nothing to fear but fear itself."')
+print()
+# This is the section for exercise 2-6
+famous_person = 'Franklin D. Roosevelt'
+print(f'{famous_person} once said "We have nothing to fear but fear itself."')
+print()
+# This is the section for exercise 2-7
+my_name = '\tFrank Taplin\n'
+print(my_name)
+print(my_name.lstrip())
+print(my_name.rstrip())
+print(my_name.strip())
