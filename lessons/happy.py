efb21cd387df05397adf16d8bae60d45393bc942
Frank Taplin <ftaplin@wsutech.edu>
2022-03-17 21:43:26 -0500

    CAC-102-13 Modules & Packages

---
+++ lessons/happy.py
@@ -0,0 +1,7 @@
+# Lesson 13 Modules & Packages
+# Basic file to demonstrating importing
+def express(is_happy=True):
+    if is_happy:
+        return 'I am happy'
+    else:
+        return 'I am not happy'
